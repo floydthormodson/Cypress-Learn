f7a82644ba825896a1e9315f9b59216a02e75911
floydthormodson <floydthormodson@outlook.com>
2021-10-19 13:06:10 -0400

    Initializes first Cypress-Learn Commit. Adds file sample_spec as my first cypress test.

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,23 @@
+// sample_spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe('My First Test', () => {
+    it('clicking "type" shows the right headings', () => {
+      cy.visit('https://example.cypress.io')
+  
+      cy.pause()
+  
+      cy.contains('type').click()
+  
+      // Should be on a new URL which includes '/commands/actions'
+      cy.url().should('include', '/commands/actions')
+  
+      // Get an input, type into it and verify that the value has been updated
+      cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com')
+    })
+  })
